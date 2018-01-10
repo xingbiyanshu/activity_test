fff4f02b37cc6cf20436ede0f0da581c0032ba2d
sissi <xingbiyanshu@gmail.com>
2018-01-10 21:19:09 +0800

    create

---
+++ app/src/main/java/com/example/sissi/activitytest/Activity1.java
@@ -0,0 +1,13 @@
+package com.example.sissi.activitytest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+    }
+}
