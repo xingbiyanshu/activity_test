cb83e5dbfd059a0c55bc9933199e9bb8737dd934
xingbiyanshu <xingbiyanshu@gmail.com>
2018-01-11 19:10:58 +0800

    add activity_2

---
+++ app/src/main/java/com/example/sissi/activitytest/Activity2.java
@@ -0,0 +1,14 @@
+package com.example.sissi.activitytest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity2 extends Activity {
+
+    /**onCreate是子activity中唯一必须实现的方法 */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2); // 必须在onCreate中调用setContentView
+    }
+}
