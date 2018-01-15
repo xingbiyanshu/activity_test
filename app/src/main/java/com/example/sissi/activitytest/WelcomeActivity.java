44fac558327e2748fedb24c15b4f4020aefdf276
xingbiyanshu <xingbiyanshu@gmail.com>
2018-01-15 15:21:16 +0800

    add WelcomeActivity

--- app/src/main/java/com/example/sissi/activitytest/WelcomeActivity.java
+++ app/src/main/java/com/example/sissi/activitytest/WelcomeActivity.java
@@ -5,23 +5,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class Activity1 extends Activity {
+public class WelcomeActivity extends Activity {
 
-    /**onCreate是子activity中唯一必须实现的方法 */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        PcTrace.p("-=->");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_1); // 必须在onCreate中调用setContentView
+        setContentView(R.layout.activity_welcome);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent();
-                intent.setAction("com.example.sissi.activitytest.ACTION2"); // 隐式匹配必须要设置action
-                intent.addCategory("android.intent.category.TEST"); // 单独的category无法匹配intent-filter，需和action配合使用　
+                intent.setAction("com.example.sissi.activitytest.ACTION1"); // 隐式匹配必须要设置action
+//                intent.addCategory("android.intent.category.TEST"); // 单独的category无法匹配intent-filter，需和action配合使用　
 //                intent.setAction("android.intent.action.VIEW"); // 一个intent只能设置一个action，后设置的会覆盖前面设置的
                 startActivity(intent);
             }
-        }, 3000);
+        }, 2000);
     }
 }
