95dfd81f29d4f880db3114b4397798c999c2b27e
xingbiyanshu <xingbiyanshu@gmail.com>
2018-01-16 19:48:03 +0800

    test launcheMode & affinity

--- app/src/main/java/com/example/sissi/activitytest/Activity2.java
+++ app/src/main/java/com/example/sissi/activitytest/Activity2.java
@@ -1,7 +1,9 @@
 package com.example.sissi.activitytest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Activity2 extends Activity {
 
@@ -11,5 +13,49 @@ public class Activity2 extends Activity {
         PcTrace.p("-=->");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2); // 必须在onCreate中调用setContentView
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent();
+                intent.setAction("com.example.sissi.activitytest.ACTION3"); // 隐式匹配必须要设置action
+                startActivity(intent);
+            }
+        }, 2000);
+    }
+
+    @Override
+    protected void onRestart() {
+        PcTrace.p("-=->");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart() {
+        PcTrace.p("-=->");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        PcTrace.p("-=->");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        PcTrace.p("-=->");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        PcTrace.p("-=->");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        PcTrace.p("-=->");
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/example/sissi/activitytest/Activity3.java
+++ app/src/main/java/com/example/sissi/activitytest/Activity3.java
@@ -1,7 +1,9 @@
 package com.example.sissi.activitytest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Activity3 extends Activity {
 
@@ -11,5 +13,13 @@ public class Activity3 extends Activity {
         PcTrace.p("-=->");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3); // 必须在onCreate中调用setContentView
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent intent = new Intent();
+//                intent.setAction("com.example.sissi.activitytest.ACTION1"); // 隐式匹配必须要设置action
+//                startActivity(intent);
+//            }
+//        }, 2000);
     }
 }
